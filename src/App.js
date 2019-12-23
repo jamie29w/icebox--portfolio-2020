6b9d9ca43043593c4c10c4273e436cbc01a960c7
Jamie Woodmancy <30516128+jamie29w@users.noreply.github.com>
2019-12-23 10:46:03 -0800

    Created NowView.js that receives dummy post data and renders a view with posts via Post.js. (#1)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React from 'react';
+import NowView from './now/NowView';
+
+import './styles/App.scss';
+import './index.scss';
 
 export default function App() {
-  return (
-    <div>
-      <h1>hello world</h1>
-    </div>
-  );
+  return <NowView />;
 }
---
+++ src/lib/dummyPostData.js
@@ -0,0 +1,26 @@
+export const dummyPosts = [
+  {
+    title: 'title 1',
+    date: new Date(),
+    body:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit quis sed, sit totam consectetur quod illo, sunt pariatur eveniet impedit veniam suscipit ipsam! Voluptatem quaerat neque id fugiat, animi eligendi!',
+  },
+  {
+    title: 'title 2',
+    date: new Date(),
+    body:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit quis sed, sit totam consectetur quod illo, sunt pariatur eveniet impedit veniam suscipit ipsam! Voluptatem quaerat neque id fugiat, animi eligendi!',
+  },
+  {
+    title: 'title 3',
+    date: new Date(),
+    body:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit quis sed, sit totam consectetur quod illo, sunt pariatur eveniet impedit veniam suscipit ipsam! Voluptatem quaerat neque id fugiat, animi eligendi!',
+  },
+  {
+    title: 'title 4',
+    date: new Date(),
+    body:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit quis sed, sit totam consectetur quod illo, sunt pariatur eveniet impedit veniam suscipit ipsam! Voluptatem quaerat neque id fugiat, animi eligendi!',
+  },
+];
