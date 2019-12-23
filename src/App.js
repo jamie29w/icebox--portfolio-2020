5ff26a7587bb879ce52fa048d8effe01e10caa77
Jamie Woodmancy <30516128+jamie29w@users.noreply.github.com>
2019-12-23 11:28:57 -0800

    Installed @reach/router. Created HomeView and PortfolioView and set up routing. (#4)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Router } from '@reach/router';
+
+import HomeView from './home/HomeView';
 import NowView from './now/NowView';
+import PortfolioView from './portfolio/PortfolioView';
 
 import './styles/App.scss';
 import './index.scss';
 
 export default function App() {
-  return <NowView />;
+  return (
+    <Router>
+      <HomeView default path='/' />
+      <NowView path='/now' />
+      <PortfolioView path='/portfolio' />
+    </Router>
+  );
 }
---
+++ src/portfolio/PortfolioView.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function PortfolioView() {
+  return (
+    <div className='main-view portfolio'>
+      <h2>Portfolio View</h2>
+    </div>
+  );
+}
