ca2b6e51dda4de804b785cfb6327e6fd6865eccc
Jamie Woodmancy <30516128+jamie29w@users.noreply.github.com>
2019-12-23 11:27:38 -0800

    Revert "Installed @reach/router. Created HomeView and PortfolioView and set up routing. (#2)" (#3)
    This reverts commit 770d0995e2c2dd08166385228f76e976c24f1886.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
 import React from 'react';
-import { Router } from '@reach/router';
-
-import HomeView from './home/HomeView';
 import NowView from './now/NowView';
-import PortfolioView from './portfolio/PortfolioView';
 
 import './styles/App.scss';
 import './index.scss';
 
 export default function App() {
-  return (
-    <Router>
-      <HomeView default path='/' />
-      <NowView path='/now' />
-      <PortfolioView path='/portfolio' />
-    </Router>
-  );
+  return <NowView />;
 }
--- src/home/HomeView.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function HomeView() {
-  return (
-    <div className='main-view home'>
-      <h2>Home View</h2>
-    </div>
-  );
-}
