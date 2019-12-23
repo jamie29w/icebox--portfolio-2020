368f5a4c65524a3a88a16d03d2b218de03ae5c23
Jamie Woodmancy <30516128+jamie29w@users.noreply.github.com>
2019-12-23 12:54:43 -0800

    Created mobile and tablet+ view base styling for HomeView.js

--- src/home/HomeView.js
+++ src/home/HomeView.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { Link } from '@reach/router';
+
+import { skillsList } from '../lib/skillsList';
+
+import '../styles/Home.scss';
 
 export default function HomeView() {
   return (
     <div className='main-view home'>
-      <h2>Home View</h2>
+      <div className='hero-grid'>
+        <h1>low</h1>
+        <h1>key</h1>
+        <h1 className='text--red'>dangerous</h1>
+      </div>
+      <div className='skills-grid'>
+        {skillsList.map(skill => (
+          <div className='skill--wrapper'>
+            <h2>{skill.title}</h2>
+            <p className='skill--detail hidden'>skill.details</p>
+          </div>
+        ))}
+        <Link className='link--discrete text--red' to='/portfolio'>
+          <h2>Let me prove it</h2>
+        </Link>
+      </div>
     </div>
   );
 }
---
+++ src/lib/skillsList.js
@@ -0,0 +1,32 @@
+export const skillsList = [
+  {
+    title: 'React',
+    details:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis aperiam esse voluptatum soluta! Quod minus, maxime voluptates itaque, fugit fugiat cum voluptate aut, tenetur magnam expedita mollitia beatae excepturi natus.',
+  },
+  {
+    title: 'Angular',
+    details:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis aperiam esse voluptatum soluta! Quod minus, maxime voluptates itaque, fugit fugiat cum voluptate aut, tenetur magnam expedita mollitia beatae excepturi natus.',
+  },
+  {
+    title: 'Greensock',
+    details:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis aperiam esse voluptatum soluta! Quod minus, maxime voluptates itaque, fugit fugiat cum voluptate aut, tenetur magnam expedita mollitia beatae excepturi natus.',
+  },
+  {
+    title: 'Material',
+    details:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis aperiam esse voluptatum soluta! Quod minus, maxime voluptates itaque, fugit fugiat cum voluptate aut, tenetur magnam expedita mollitia beatae excepturi natus.',
+  },
+  {
+    title: 'Bootstrap',
+    details:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis aperiam esse voluptatum soluta! Quod minus, maxime voluptates itaque, fugit fugiat cum voluptate aut, tenetur magnam expedita mollitia beatae excepturi natus.',
+  },
+  {
+    title: 'HTML5 + CSS3',
+    details:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis aperiam esse voluptatum soluta! Quod minus, maxime voluptates itaque, fugit fugiat cum voluptate aut, tenetur magnam expedita mollitia beatae excepturi natus.',
+  },
+];
