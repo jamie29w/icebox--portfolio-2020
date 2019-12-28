de4e36528862f9b5312d56554dd695d8d2a661d6
Jamie Woodmancy <30516128+jamie29w@users.noreply.github.com>
2019-12-28 15:09:04 -0800

    Added nav component + refactors and bug fixes

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import { Router } from '@reach/router';
 import HomeView from './home/HomeView';
 import NowView from './now/NowView';
 import PortfolioView from './portfolio/PortfolioView';
+import Nav from './nav/Nav';
 
 import './styles/App.scss';
 import './index.scss';
 
 export default function App() {
   return (
-    <Router>
-      <HomeView default path='/' />
-      <NowView path='/now' />
-      <PortfolioView path='/portfolio' />
-    </Router>
+    <div className='main-view--wrapper'>
+      <Router className='main-router'>
+        <HomeView default path='/' />
+        <NowView path='/now' />
+        <PortfolioView path='/portfolio' />
+      </Router>
+      <Nav />
+    </div>
   );
 }
---
+++ src/nav/Nav.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from '@reach/router';
+
+import '../styles/Nav.scss';
+
+export default function Nav() {
+  return (
+    <div className='nav-wrapper'>
+      <a href='https://github.com/jamie29w' target='_blank' rel='noopener'>
+        <i class='fab fa-github fa-2x'></i>
+      </a>
+      <Link to='/'>skills</Link>
+      <Link to='/now'>now</Link>
+      <Link to='/portfolio'>portfolio</Link>
+      <a
+        href='https://www.linkedin.com/in/jamiewoodmancy/'
+        target='_blank'
+        rel='noopener'
+      >
+        <i class='fab fa-linkedin-in fa-2x'></i>
+      </a>
+    </div>
+  );
+}
