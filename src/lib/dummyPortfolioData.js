feb124c82d98f7d476f910bd8347e6f007c468cb
Jamie Woodmancy <jamie29w@gmail.com>
2019-12-28 15:12:34 -0800

    Created mobile layout for portfolio view
    added box-sizing border-box to everything in index.scss
    
    wip: portfolio layout
    
    removed Nav from App.js. Will add it to each component separately so it can be styled accordingly
    
    added scrolling middle section to portfolio grid
    
    Importing actual images from public/assets. Also fixed linter issues
    
    re-ordered portfolio data
    
    portfolio and nav updates (forgot some of the changes made)

---
+++ src/lib/dummyPortfolioData.js
@@ -0,0 +1,44 @@
+export const portfolioData = [
+  {
+    title: 'Snap CRM',
+    images: [
+      {
+        src: './assets/snap-crm/snap-crm-mobile-main.png',
+        alt: 'snap crm mobile main',
+      },
+      {
+        src: './assets/snap-crm/snap-crm-mobile-create.png',
+        alt: 'snap crm mobile create',
+      },
+      {
+        src: './assets/snap-crm/snap-crm-desktop-main.png',
+        alt: 'snap crm desktop main',
+      },
+      {
+        src: './assets/snap-crm/snap-crm-desktop-modal.png',
+        alt: 'snap crm desktop modal',
+      },
+    ],
+    details:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque a doloremque animi quam laboriosam corporis, totam dolorum eaque saepe eligendi nostrum error temporibus molestiae amet officiis eum perferendis debitis illum.',
+  },
+  {
+    title: 'Native Weather',
+    images: [
+      {
+        src: './assets/native-weather/native-weather-login.png',
+        alt: 'native weather login',
+      },
+      {
+        src: './assets/native-weather/native-weather-today.png',
+        alt: 'native weather today',
+      },
+      {
+        src: './assets/native-weather/native-weather-forecast.png',
+        alt: 'native weather forecast',
+      },
+    ],
+    details:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque a doloremque animi quam laboriosam corporis, totam dolorum eaque saepe eligendi nostrum error temporibus molestiae amet officiis eum perferendis debitis illum.',
+  },
+];
--- src/portfolio/PortfolioView.js
+++ src/portfolio/PortfolioView.js
@@ -1,9 +1,49 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { TimelineMax } from 'gsap/all';
+
+import Nav from '../nav/Nav';
+import '../styles/Portfolio.scss';
+
+import { portfolioData } from '../lib/dummyPortfolioData';
 
 export default function PortfolioView() {
+  const portfolioWrapperRef = useRef(null);
+
+  useEffect(() => {
+    const fadeInTimeline = new TimelineMax();
+
+    fadeInTimeline.fromTo(
+      portfolioWrapperRef.current,
+      0.5,
+      { opacity: 0.5 },
+      { opacity: 1 }
+    );
+  }, []);
+
   return (
-    <div className='main-view portfolio'>
-      <h2>Portfolio View</h2>
+    <div ref={portfolioWrapperRef} className='portfolio--wrapper'>
+      <h1>Personal Projects</h1>
+      {portfolioData.length && (
+        <div className='projects-grid'>
+          {portfolioData.map(project => (
+            <div id={project.title} key={project.title}>
+              <h2>{project.title}</h2>
+              {project.images.map(image => {
+                return (
+                  <img
+                    className='project--image'
+                    src={image.src}
+                    alt={image.alt}
+                    key={image.src}
+                  />
+                );
+              })}
+              <p>{project.details}</p>
+            </div>
+          ))}
+        </div>
+      )}
+      <Nav />
     </div>
   );
 }
